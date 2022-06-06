1afe5226f6f91ce6919b3ea4e5a3b651f40a7d89
drmrhdt <drmrhdt@gmail.com>
2022-06-06 13:12:48 +0300

    feat(CYBER-34): resolve task

---
+++ src/database/mocks/news.js
@@ -0,0 +1,76 @@
+'use strict';
+
+module.exports = {
+  mockNews: [
+    {
+      createdAt: '2022-06-04T20:33:43.912Z',
+      description: 'Новость 1',
+      main_image_url: null,
+      title: 'Новость 1',
+      updatedAt: '2022-06-04T19:33:43.912Z',
+    },
+    {
+      createdAt: '2022-06-04T20:33:43.912Z',
+      description: 'Новость 2',
+      main_image_url: null,
+      title: 'Новость 2',
+      updatedAt: '2022-06-04T18:33:43.912Z',
+    },
+    {
+      createdAt: '2022-06-04T20:33:43.912Z',
+      description: 'Новость 3',
+      main_image_url: null,
+      title: 'Новость 3',
+      updatedAt: '2022-06-04T17:33:43.912Z',
+    },
+    {
+      createdAt: '2022-06-04T20:33:43.912Z',
+      description: 'Новость 4',
+      main_image_url: null,
+      title: 'Новость 4',
+      updatedAt: '2022-06-04T16:33:43.912Z',
+    },
+    {
+      createdAt: '2022-06-04T20:33:43.912Z',
+      description: 'Новость 5',
+      main_image_url: null,
+      title: 'Новость 5',
+      updatedAt: '2022-06-04T15:33:43.912Z',
+    },
+    {
+      createdAt: '2022-06-04T14:33:43.912Z',
+      description: 'Новость 6',
+      main_image_url: null,
+      title: 'Новость 6',
+      updatedAt: '2022-06-04T20:33:43.912Z',
+    },
+    {
+      createdAt: '2022-06-04T13:33:43.912Z',
+      description: 'Новость 7',
+      main_image_url: null,
+      title: 'Новость 7',
+      updatedAt: '2022-06-04T20:33:43.912Z',
+    },
+    {
+      createdAt: '2022-06-04T12:33:43.912Z',
+      description: 'Новость 8',
+      main_image_url: null,
+      title: 'Новость 8',
+      updatedAt: '2022-06-04T11:33:43.912Z',
+    },
+    {
+      createdAt: '2022-06-04T10:33:43.912Z',
+      description: 'Новость 9',
+      main_image_url: null,
+      title: 'Новость 9',
+      updatedAt: '2022-06-04T09:33:43.912Z',
+    },
+    {
+      createdAt: '2022-06-04T08:33:43.912Z',
+      description: 'Новость 10',
+      main_image_url: null,
+      title: 'Новость 10',
+      updatedAt: '2022-06-04T20:33:43.912Z',
+    },
+  ],
+};
